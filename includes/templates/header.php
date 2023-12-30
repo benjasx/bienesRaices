bef57666ec11eaa4219693d03ba4bc83f157a872
benjasx <benjasanchez175@gmail.com>
2023-12-29 20:05:14 -0700

    avance pendiente

---
+++ includes/templates/header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienes raices</title>
+    <link rel="stylesheet" href="build/css/app.css">
+</head>
+<body>
+    
+    <header class="header inicio">
+        <div class="contenedor contenido-header">
+            <div class="barra">
+                <a href="/">
+                    <img src="build/img/logo.svg" alt="Logo">
+                </a>
+                
+                
+
+                <div class="navdark">
+                    <div class="mobile-menu">
+                        <img src="build/img/barras.svg" alt="menu-Hamburgesas">
+                    </div>
+                    <nav class="navegacion">
+                        <a href="nosotros.html">Nosotros</a>
+                        <a href="anuncios.html">Anuncios</a>
+                        <a href="blog.html">Blog</a>
+                        <a href="contacto.html">Contacto</a>
+                    </nav>
+
+                    <div class="dark-mode-btn">
+                        <img src="build/img/dark-mode.svg" alt="Dark-Mode">
+                    </div>
+                </div>
+                
+
+                
+            </div> <!-- Cierre de la barra -->
+
+            <h1>Venta de casas y departamentos exclucivos de lujo 🏡</h1>
+          
+        </div>
+    </header>
