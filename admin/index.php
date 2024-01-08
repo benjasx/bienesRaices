d620996025a3eee6a0bebb7408f2eef09000d2d7
benjasx <benjasanchez175@gmail.com>
2024-01-08 02:04:50 -0700

    ready for read

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,11 @@
 <?php
-    
+    require '../includes/config/database.php';
+    $db = conectarDB();
+
+    $query = "SELECT * FROM propiedades";
+
+    $resultadoDB = mysqli_query($db, $query);
+
     $resultado = $_GET['resultado'] ?? null;
     require '../includes/funciones.php';
     incluirTemplate('header');
@@ -11,11 +17,38 @@
             <p class="alerta exito"> Anuncio creado correctamente</p>
         <?php endif; ?>
         <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva propiedad</a>
+
+        <table class="propiedades">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>TITULO</th>
+                    <th>IMAGEN</th>
+                    <th>PRECIO</th>
+                    <th>ACCIONES</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php while($propiedad = mysqli_fetch_assoc($resultadoDB)): ?>
+                <tr>
+    
+                    <td><?php echo $propiedad['id']?></td>
+                    <td><?php echo $propiedad['titulo']?></td>
+                    <td><img class="imagen-tabla" src="/imagenes/<?php echo $propiedad['imagen']?>" alt="imagen"></td>
+                    <td><?php echo "$".$propiedad['precio']?></td>
+                    <td class="acciones">
+                        <a href="#" class="boton-rojo-block btn">ELIMINAR</a>
+                        <a href="#" class="boton-amarillo-block btn">ACTUALIZAR</a>
+                    </td>
+                </tr>
+                <?php endwhile; ?>
+            </tbody>
+        </table>
     </main>
 
     <?php
+        mysqli_close($db);
         incluirTemplate('footer');
     ?>
-    <script src="build/js/bundle.min.js"></script>
 </body>
 </html>
